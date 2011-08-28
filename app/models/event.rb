ec7b360fd589da5b8607cc8cf4d36609701441b9
Eric Hayes <eric@erichayes.net>
2011-08-27 23:44:37 -0700

    lil bit o design

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,20 +8,16 @@ class Event < ActiveRecord::Base
     end
   end
   
-  def initialize
-    super()
-  end
-  
   def fdate
-    date.strftime("%A, %b %d") unless date.nil?
+    start_time.strftime("%A, %b %d") unless date.nil?
   end
   
-  def ftime(time)
-    if time == 'start'
-      start_time.strftime("%l:%M %p") unless start_time.nil?
-    else
-      end_time.strftime("%l:%M %p") unless end_time.nil?
-    end
+  def fstart
+    start_time.strftime("%l:%M %p") unless start_time.nil?
+  end
+  
+  def fend
+    end_time.strftime("%l:%M %p") unless end_time.nil?
   end
   
 end
