f74cc9905ca7b25a962eac1a71dd7af731a073f0
Eric Hayes <eric@erichayes.net>
2012-01-11 11:34:47 -0800

    event mostly themed

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -3,7 +3,15 @@
 
 $(document).ready(function(){
    $("a.drawer_toggle").click(function(){
-       $(this).parent(".event").find(".event_drawer").slideToggle();
-       $(this).parent(".event").find(".twirl").toggleClass("down");
+       // $(this).parent(".event").find(".event_drawer").slideToggle();
+       // $(this).parent(".event").find(".twirl").toggleClass("down");
+       openDrawer($(this));
    });
+   //$(".event:first .event_drawer").slideToggle();
+   //openDrawer($(".event:first a.drawer_toggle"));
 });
+
+function openDrawer(element){
+    element.parent(".event").find(".event_drawer").slideToggle();
+    element.parent(".event").find(".twirl").toggleClass("down");
+}
